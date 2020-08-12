04d43faa4c1ad4ced37540d46877e6113c914eee
Ragib <ahashanragib@gmail.com>
2020-08-12 01:36:00 -0400

    Adding why choose nasta

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -149,7 +149,7 @@ const HomePage = () => {
 
             
             <div style={{
-                backgroundImage: `linear-gradient(to right, rgba(0,0,0, 0.5) 10%, rgba(0,0,0, 0) 100%), url(${FirstImage})`,
+                backgroundImage: `linear-gradient(to right, rgba(0,0,0, 0.8) 20%, rgba(0,0,0, 0) 100%), url(${FirstImage})`,
                 backgroundRepeat: 'no-repeat',
                 height:'100%'
                 }}>
@@ -163,57 +163,65 @@ const HomePage = () => {
                         </h1>
 
                         <h4 id='low-message'>
-                            {'We partner with your favorite restaurants in order to facilitate subscription based orders'}
+                            {/* {'We partner with your favorite restaurants in order to facilitate subscription based orders'} */}
+                            Eat delicious meals today, tomorrow and everyday from your favorite places
                         </h4>
-                    </div>
                         
 
                         <Button 
                             id='order-now'
+                            style={{marginTop:'30px'}}
                             onClick={()=>{history.push("/order-food");}}
-                        > {'Order now'} </Button>
-
-
-                {/* <div class="d-flex justify-content-center" >
-                    <h1 class='grand-message' style={{position:'absolute'}}>
-                        {'Meal plans from your favorite restaurant.'}
-                    </h1>
-                </div>
-                <div class="d-flex justify-content-end">
-                    <img src={LandingPageImage} alt='cow' style={{marginTop:'10%', marginRight:'25%', width:'800px'}}/>
-                </div> */}
+                            > {'Get started'} </Button>
+                    </div>
             </section>
             </div>
             <section class='view-screen' id='second-view' data-aos="fade-up">    
-            <div class="d-flex justify-content-center" style={{marginBottom:'8px'}}>
-                
-                    <h1 style={{
-                        // position: 'absolute', 
-                        // left: '50%', 
-                        // top: '0%',
-                        // transform: 'translate(-50%, 0%)'
-                    }}> It's simple. </h1>
-            </div>
-            <div class="d-flex justify-content-center" style={{marginBottom:'100px'}}>
-                    <p style={{
-                        // position: 'absolute', 
-                        // left: '50%', 
-                        // top: '10%',
-                        // color:"rgb(0, 0, 0, 0.8)",
-                        // transform: 'translate(-50%, -100%)'
-                    }}> Just how ordering your food should be </p>
-            </div>
-
-
-
+            <div class="align-middle">
+                <div class="d-flex justify-content-center" style={{marginBottom:'8px'}}>
+                    
+                        <h1 style={{fontSize:'100px'}}> How it works </h1>
+                </div>
+                <div class="d-flex justify-content-center" style={{marginBottom:'100px'}}>
+                        <h3> Easy, just how ordering your food should be. </h3>
+                </div>
                 <ShowStepsOrdering />
-
-
-
-                
+            </div>
             </section>
 
-            <section 
+
+            <div style={{backgroundColor:'#e8ba5a', color: 'white', paddingTop:'100px', paddingBottom:'100px'}} >
+                <div class="d-flex justify-content-center" >
+                    <h1 style={{fontSize:'90px'}}> Why use Nasta? </h1>
+                </div>
+                <br /> 
+                <div class='row' >
+                    <div class='col'>
+                        <div class="d-flex justify-content-center" >
+                            <img src={PickFoods} alt='brouse' style={{width:'40%'}} />
+                        </div>
+                    </div>
+                    <div class='col' >
+                        <h1 style={{fontSize:'26px'}}> <span style={{color:'#519ea4'}}> √ </span> Don’t cook, Feast </h1>
+                        <p style={{width:'500px', fontSize:'20px', paddingLeft:'0px'}}> No more browsing recipes online or wondering what you should eat everyday.
+                            Schedule your meals with Nasta and enjoy your meals. </p>
+                        <br /> <br />
+
+
+                        <h1 style={{fontSize:'26px'}}> <span style={{color:'#519ea4'}}> √ </span> Save yourself the burden of groceries shopping </h1>
+                        <p style={{width:'500px', fontSize:'20px', paddingLeft:'0px'}}> Shop less, you’ll have more time to focus on the important things. </p>
+                        <br /> <br />
+
+
+                        <h1 style={{fontSize:'26px'}}> <span style={{color:'#519ea4'}}> √ </span> Delicious food </h1>
+                        <p style={{width:'500px', fontSize:'20px', paddingLeft:'0px'}}> With Nasta, you will feast everyday, twice a day the best meals in your city. </p>
+                        <br /> <br />
+
+                    </div>
+                </div>
+            </div>
+
+            {/* <section 
                 style={{
                 marginBottom:'300px',
                 marginTop:`${queryLarge ? '-500px' : ''}`
@@ -275,12 +283,12 @@ const HomePage = () => {
 
                 
 
-            </section>
+            </section> */}
 
             <div id='container' 
                 style={{
-                    backgroundColor:'#FD488E',
-                    marginTop:'-200px'
+                    backgroundColor:'white',
+                    marginTop:'0px'
                 }}
             >
                 <Button id='notified-btn' style={{marginTop:'50px', marginBottom:'120px', borderColor:'black'}}> {'Order now!'} </Button>
